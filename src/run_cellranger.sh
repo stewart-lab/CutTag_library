31d3015ae23eea66cebe02f94e03a7dd92aca9c7
bmmoore43 <bmjohns43@gmail.com>
2024-09-23 15:25:14 -0500

    wip

--- src/run_cellranger.sh
+++ src/run_cellranger.sh
@@ -1,5 +1,5 @@
 ## Get or build reference
-# mm10_ref="/projects/ps-renlab/y2xie/projects/genome_ref/refdata-cellranger-arc-mm10-2020-A-2.0.0"
+mm10_ref="/w5home/bmoore/cut_and_tag/mouse_mm10/refdata-cellranger-arc-mm10-2020-A-2.0.0"
 
 ## Demultiplex
 # cellranger-arc mkfastq --run ${bcl2_dir} --csv ${script_dir}/SampleSheet.atac.csv --output-dir ${fastq_dir}
