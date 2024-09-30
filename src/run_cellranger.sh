3340dae63edeff2275501bbbd03347f25f6d569e
bmmoore43 <bmjohns43@gmail.com>
2024-09-30 12:51:43 -0500

    adding config options to process_scCutTag.rmd

--- src/run_cellranger.sh
+++ src/run_cellranger.sh
@@ -1,7 +1,7 @@
 ## Get or build reference
 mm10_ref="/w5home/bmoore/cut_and_tag/mouse_mm10/refdata-cellranger-arc-mm10-2020-A-2.0.0"
 fastq_dir="/w5home/bmoore/cut_and_tag/GSE224560/fastqs/"
-s="FC_H3K27ac"
+s="FC_H3K27ac" # sample id
 ## Demultiplex
 # cellranger-arc mkfastq --run ${bcl2_dir} --csv ${script_dir}/SampleSheet.atac.csv --output-dir ${fastq_dir}
 
